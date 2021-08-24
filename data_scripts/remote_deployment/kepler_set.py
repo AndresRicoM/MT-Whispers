7d816cbea24de0d330331e70adaa9a1412114be0
aricom <aricom@mit.edu>
2021-08-24 17:02:56 -0400

    added porto and lisbon data

--- data_scripts/remote_deployment/kepler_set.py
+++ data_scripts/remote_deployment/kepler_set.py
@@ -1,12 +1,14 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 import os
 import math
 import datetime
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()  # for plot styling
+from matplotlib import gridspec
 
-path = '../../collected_data_2/raw/ski1_andorra_D1.txt'
-destin_path = '../../collected_data_2/processed/ski1_andorra_D1.csv'
+path = '../../collected_data_2/raw/porto.txt'
+destin_path = '../../collected_data_2/processed/porto.csv'
 data_labels = ['datetime', 'millis', 'lat', 'coordlat', 'lon', 'coordlon','pressure','altitude', 'temp']
 
 input_data = np.genfromtxt(path, delimiter = ',', dtype='str',  invalid_raise=False) #,  dtype='str')
@@ -14,22 +16,29 @@ input_data = np.genfromtxt(path, delimiter = ',', dtype='str',  invalid_raise=Fa
 #for rows in
 
 
-latitudes = input_data[:,2].astype(np.float)
-longitudes= input_data[:,4].astype(np.float)
+latitudes = input_data[:,2].astype(float)
+longitudes= input_data[:,4].astype(float)
 
 #Convert coordinates
 
-for rows in range(latitudes.shape[0]):
+for rows in range(1):#latitudes.shape[0]):
 
+    #print(latitudes[rows])
     lat = latitudes[rows] * .01
+    #print(lat)
     lat_deg = math.floor(lat)
+    #print(lat_deg)
     lat_min = ((lat - lat_deg) * 100) / 60
+    #print(lat_min)
     dd_lat = lat_deg + lat_min
+    #print(dd_lat)
 
+    print(longitudes[rows])
     lon = longitudes[rows] * .01
     lon_deg = math.floor(lon)
     lon_min = ((lon - lon_deg) * 100) / 60
     dd_lon =  (lon_deg + lon_min)
+    print(dd_lon)
 
     latitudes[rows] = dd_lat
     longitudes[rows] = dd_lon
@@ -37,12 +46,31 @@ for rows in range(latitudes.shape[0]):
 input_data[:,2] = latitudes[:]
 input_data[:,4] = longitudes[:]
 
-print(latitudes.shape)
-
-#Adjust GPS Decimals
-
 #Join Independent data sets.
 output_data = np.vstack([data_labels, input_data])
-
-print(output_data)
 np.savetxt(destin_path, output_data, delimiter = ',', fmt = '%s')
+
+#Visualization
+plt.style.use('dark_background')
+
+gs = gridspec.GridSpec(3,1)
+fig = plt.figure()
+dot_size = 3
+
+
+ax = fig.add_subplot(gs[0])
+ax.plot(input_data[:,1].astype(float),input_data[:,6].astype(float), 'cyan')
+ax.set_ylabel(r'Pressure (Pa)', size =16)
+
+ax = fig.add_subplot(gs[1])
+ax.plot(input_data[:,1].astype(float),input_data[:,7].astype(float), 'magenta')
+ax.set_ylabel(r'Altitude (m)', size =16)
+
+ax = fig.add_subplot(gs[2])
+ax.plot(input_data[:,1].astype(float),input_data[:,8].astype(float), 'yellow')
+ax.set_ylabel(r'Temperature (C)', size =16)
+ax.set_xlabel(r'Time (millis)', size =16)
+
+fig.suptitle('Andorra Test Deployment', fontsize=32)
+
+#plt.show()
